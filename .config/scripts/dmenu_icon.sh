c26de6497d00207c9f907f40af151f82e48250dd
yuandi42 <realyuandi42@gmail.com>
2022-02-10 11:24:12 +0800

    Add my scripts

---
+++ .config/scripts/dmenu_icon.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Give dmenu list of characters in unicode PUA to copy.
+# ttf-all-the-icons fonts are needed.
+
+sed "s/\s*[E-F][A-F0-9][A-F0-9][A-F0-9]//" ~/.config/scripts/icon_list.txt | dmenu -i -l 10 -fn sans-16 -p "Select:" | awk '{print $1}'| tr -d '\n' | xclip -selection clipboad
