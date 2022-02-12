40dcac8fb20f060042c61e9596d53de41491b5a0
yuandi42 <realyuandi42@gmail.com>
2022-02-12 21:45:26 +0800

    remove xwallpapaer

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 picom -b
-xwallpaper --zoom ~/.config/wallpaper/wallpaper00.png&
 
-bash -c sxhkd -c ~/.config/sxhkd/sxhkdrc&
+sxhkd -c ~/.config/sxhkd/sxhkdrc&
 fcitx5 -d --disable wayland,waylandim --enable keyboard,clipboard,unicode,xim,xcb,classui,notificationitem,notificatoins,rime&
