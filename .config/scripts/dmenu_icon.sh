46362cc0334fe0375185760d1b8a0a6c46ee2ad1
yuandi42 <realyuandi42@gmail.com>
2022-02-13 12:07:51 +0800

    revised regex

--- .config/scripts/dmenu_icon.sh
+++ .config/scripts/dmenu_icon.sh
@@ -2,4 +2,4 @@
 # Give dmenu list of characters in unicode PUA to copy.
 # ttf-all-the-icons fonts are needed.
 
-sed "s/\s*[E-F][A-F0-9][A-F0-9][A-F0-9]//" ~/.config/scripts/icon_list.txt | dmenu -i -l 10 -fn sans-16 -p "Select:" | awk '{print $1}'| tr -d '\n' | xclip -selection clipboad
+sed "s/\s*[E-F][A-F0-9]\{3\}//" ~/.config/scripts/icon_list.txt | dmenu -i -l 10 -fn sans-16 -p "Select:" | awk '{print $1}'| tr -d '\n' | xclip -selection clipboad
