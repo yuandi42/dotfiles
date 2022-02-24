b3953abe29d069164d2e90e6df52a5ec855fb008
yuandi42 <realyuandi42@gmail.com>
2022-02-25 01:40:01 +0800

    haven't tested yet

---
+++ .vim/plugin_setup.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/sh
+# auto setup my vim plug-in, haven't tested anyway.
+mkdir -p '~/.vim/pack/default/start'
+cd '~/.vim/pack/default/start'
+git clone https://github.com/vim-airline/vim-airline.git
