9da118da0547789752e78de7de8d452d1712d405
yuandi42 <realyuandi42@gmail.com>
2022-02-26 02:30:13 +0800

    add vim-startify

--- .vim/plugin_setup.sh
+++ .vim/plugin_setup.sh
@@ -3,6 +3,7 @@
 mkdir -p '~/.vim/pack/default/start'
 cd '~/.vim/pack/default/start'
 git clone https://github.com/vim-airline/vim-airline.git
-vim -u NONE -c "helptags ~/.vim/pack/default/start/vim-airline/doc" -c q
 git clone https://github.com/preservim/nerdtree.git
+git clone https://github.com/mhinz/vim-startify.git
+vim -u NONE -c "helptags ~/.vim/pack/default/start/vim-airline/doc" -c q
 vim -u NONE -c "helptags ~/.vim/pack/default/start/nerdtree/doc" -c q
