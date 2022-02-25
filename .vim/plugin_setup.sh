fb3fa8878507e3f38c87e486224298d25876be17
yuandi42 <realyuandi42@gmail.com>
2022-02-25 17:10:56 +0800

    add NERDTree

--- .vim/plugin_setup.sh
+++ .vim/plugin_setup.sh
@@ -3,3 +3,6 @@
 mkdir -p '~/.vim/pack/default/start'
 cd '~/.vim/pack/default/start'
 git clone https://github.com/vim-airline/vim-airline.git
+vim -u NONE -c "helptags ~/.vim/pack/default/start/vim-airline/doc" -c q
+git clone https://github.com/preservim/nerdtree.git
+vim -u NONE -c "helptags ~/.vim/pack/default/start/nerdtree/doc" -c q
