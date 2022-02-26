1cd3deea22357ef709805811f18a727f3ea0c9b4
yuandi42 <realyuandi42@gmail.com>
2022-02-26 23:18:03 +0800

    add more plugins

--- .vim/plugin_setup.sh
+++ .vim/plugin_setup.sh
@@ -6,5 +6,8 @@ git clone https://github.com/vim-airline/vim-airline.git
 git clone https://github.com/preservim/nerdtree.git
 git clone https://github.com/mhinz/vim-startify.git
 git clone https://github.com/rhysd/vim-healthcheck.git
+git clone https://github.com/ryanoasis/vim-devicons.git
+git clone https://github.com/tiagofumo/vim-nerdtree-syntax-highlight.git
+
 vim -u NONE -c "helptags ~/.vim/pack/default/start/vim-airline/doc" -c q
 vim -u NONE -c "helptags ~/.vim/pack/default/start/nerdtree/doc" -c q
