008ebff3620e31d781ad107d4b1595870d94fd72
yuandi42 <realyuandi42@gmail.com>
2022-02-27 03:04:43 +0800

    add plug-ins loading order control, helptags generation, WIP

--- .vim/plugin_setup.sh
+++ .vim/plugin_setup.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/sh
-# auto setup my vim plug-in, haven't tested anyway.
+# auto setup my vim plug-in, hasn't tested anyway. WIP.
+
+# set up for start plug-ins, i.e., plug-ins which are added everytime my vim
+# starts.
 mkdir -p '~/.vim/pack/default/start'
 cd '~/.vim/pack/default/start'
 git clone https://github.com/vim-airline/vim-airline.git
@@ -9,5 +12,13 @@ git clone https://github.com/rhysd/vim-healthcheck.git
 git clone https://github.com/ryanoasis/vim-devicons.git
 git clone https://github.com/tiagofumo/vim-nerdtree-syntax-highlight.git
 
-vim -u NONE -c "helptags ~/.vim/pack/default/start/vim-airline/doc" -c q
-vim -u NONE -c "helptags ~/.vim/pack/default/start/nerdtree/doc" -c q
+# rename the plug-in directories. since built-in plugin manager load plug-ins in
+# alphabetical order, I can control the loading order by renaming them.
+mv -R vim-devicons z-vim-devicons 
+# mv -R blabla a-blabla
+
+# generate helptags for those plug-ins
+for p in '~/.vim/pack/default/start/*'
+do
+    (vim -u NONE -c "helptags $p/doc" -c q)
+done
