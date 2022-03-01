e10ea2037bc20ec1945bada737c0d5f74116eb4c
yuandi42 <realyuandi42@gmail.com>
2022-03-02 01:19:26 +0800

    I think the work is done.

--- .config/scripts/dmenu_icon.sh
+++ .config/scripts/dmenu_icon.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/sh
 # Give dmenu list of characters in unicode PUA to copy.
-# Well, now i have to face a serious fonts confilct since I've
-# installed fxxking too many '''icon fonts'''. Since I am wasting my
-# time to figure out how to deal with those pieces of bullshit,
-# this tiny cute script can't actually work, i.e. WIP. Dxmn.
+# HAHAHA! I installed nerd fonts and threw away emacs-all-the-icons. Using tons
+# of vim macors and regex subsititute, I finally get the work doe.
+# P.S.: only list unicode in PUA, i.e., E000-F8FF.
 
-sed "s/\s*[E-F][A-F0-9]\{3\}//" ~/.config/scripts/icon_list.txt | dmenu -i -l 10 -fn sans-16 -p "Select:" | awk '{print $1}'| tr -d '\n' | xclip -selection clipboad
+sed "s/\s*[E-F][A-F0-9]\{3\}//" ~/.config/scripts/nerd_icons_list.txt | dmenu -i -l 10 -fn sans-16 -p "Select:" | awk '{print $1}'| tr -d '\n' | xclip -selection clipboad
