7908faed42b0f8848cec8680cd6b54fa6cccdaea
yuandi42 <realyuandi42@gmail.com>
2022-03-03 18:15:57 +0800

    Adapted for my new dmenu.

--- .config/scripts/dmenu_SNR.sh
+++ .config/scripts/dmenu_SNR.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/sh
 # select shutdown or reboot in dmenu.
 
-var1=$(echo -e 'Reboot\nShutdown'| dmenu -i -p 'Run:' -fn sans-16)
+var1=$(echo -e 'Reboot\nShutdown'| dmenu -c -i -l 2 -bw 2 -fn sans-16)
 case $var1 in 
 Shutdown)
-  var2=$(echo -e 'No\nYes'| dmenu -i -p 'Sure?' -fn sans-16)
+  var2=$(echo -e 'No\nYes'| dmenu -c -i -p 'Sure?' -fn sans-16)
   if [ $var2 = Yes ]; then shutdown -h now; fi;;
 Reboot)
-  var2=$(echo -e 'No\nYes'| dmenu -i -p 'Sure?' -fn sans-16)
+  var2=$(echo -e 'No\nYes'| dmenu -c -i -p 'Sure?' -fn sans-16)
   if [ $var2 = Yes ]; then reboot; fi;;
 esac
--- .config/scripts/dmenu_icon.sh
+++ .config/scripts/dmenu_icon.sh
@@ -4,4 +4,8 @@
 # of vim macors and regex subsititute, I finally get the work doe.
 # P.S.: only list unicode in PUA, i.e., E000-F8FF.
 
-sed "s/\s*[E-F][A-F0-9]\{3\}//" ~/.config/scripts/nerd_icons_list.txt | dmenu -i -l 10 -fn sans-16 -p "Select:" | awk '{print $1}'| tr -d '\n' | xclip -selection clipboad
+sed "s/\s*[E-F][A-F0-9]\{3\}//" ~/.config/scripts/nerd_icons_list.txt | 
+\dmenu -i -bw 2 -c -l 10 -fn sans-16 -p "Select:" | 
+\awk '{print $1}'| 
+\tr -d '\n' | 
+\xclip -selection clipboad
