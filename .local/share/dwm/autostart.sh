07e8bdeef4f15066a3b12df08635bd9ef4a7a192
yuandi42 <realyuandi42@gmail.com>
2022-03-22 01:12:04 +0800

    autostart script for dwm.

---
+++ .local/share/dwm/autostart.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+picom --experimental-backends -b
+sxhkd -c ~/.config/sxhkd/sxhkdrc&
+fcitx5 -d --disable wayland,waylandim --enable keyboard,clipboard,unicode,xim,xcb,classui,notificationitem,notificatoins,rime&
+dwmblocks&
