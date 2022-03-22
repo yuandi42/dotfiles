3a51140516ce6e3017f7d61637c3e69e9bbfc407
yuandi42 <realyuandi42@gmail.com>
2022-03-22 23:30:17 +0800

    add conky

--- .local/share/dwm/autostart.sh
+++ .local/share/dwm/autostart.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-picom --experimental-backends -b
 sxhkd -c ~/.config/sxhkd/sxhkdrc&
+picom --experimental-backends -b
 fcitx5 -d --disable wayland,waylandim --enable keyboard,clipboard,unicode,xim,xcb,classui,notificationitem,notificatoins,rime&
 dwmblocks&
+conky&
