6f09c76f2ecd8fdcd594da3c1e417f317c571b9c
yuandi42 <realyuandi42@gmail.com>
2022-03-22 23:31:58 +0800

    remove qtile stuff

--- .config/qtile/autostart.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-picom --experimental-backends -b
-
-sxhkd -c ~/.config/sxhkd/sxhkdrc&
-fcitx5 -d --disable wayland,waylandim --enable keyboard,clipboard,unicode,xim,xcb,classui,notificationitem,notificatoins,rime&
