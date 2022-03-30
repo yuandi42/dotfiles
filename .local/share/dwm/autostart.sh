5a7614676f504a5df51fbba29270b5713efad582
yuandi42 <realyuandi42@gmail.com>
2022-03-31 03:16:17 +0800

    add autostart

--- .local/share/dwm/autostart.sh
+++ .local/share/dwm/autostart.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+sxhkd -c ~/.config/sxhkd/sxhkdrc&
 picom --experimental-backends -b
 fcitx5 -d --disable wayland,waylandim --enable keyboard,clipboard,unicode,xim,xcb,classui,notificationitem,notificatoins,rime&
 dwmblocks&
 conky&
+alacritty
