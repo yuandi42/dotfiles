5228cc65c34c6a57f4c5cf6d182bfca77a4a5551
yuandi42 <realyuandi42@gmail.com>
2022-04-06 19:35:02 +0800

    autostart scratchpad

--- .local/share/dwm/autostart.sh
+++ .local/share/dwm/autostart.sh
@@ -4,4 +4,4 @@ picom --experimental-backends -b
 fcitx5 -d --disable wayland,waylandim --enable keyboard,clipboard,unicode,xim,xcb,classui,notificationitem,notificatoins,rime&
 dwmblocks&
 conky&
-alacritty
+xsetroot -name "fsignal:1"
