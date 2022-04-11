e2b488c50867160ef2b3f6ed7ae923b5ba766930
yuandi42 <realyuandi42@gmail.com>
2022-04-11 22:59:30 +0800

    who needs conky?

--- .local/share/dwm/autostart.sh
+++ .local/share/dwm/autostart.sh
@@ -3,5 +3,4 @@ sxhkd -c ~/.config/sxhkd/sxhkdrc&
 picom --experimental-backends -b
 fcitx5 -d --disable wayland,waylandim --enable keyboard,clipboard,unicode,xim,xcb,classui,notificationitem,notificatoins,rime&
 dwmblocks&
-conky&
-xsetroot -name "fsignal:1"
+# conky&
