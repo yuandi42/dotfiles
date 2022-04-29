4fdcd46380ae926768ac8e13793604d2460871b8
yuandi42 <realyuandi42@gmail.com>
2022-04-30 02:07:43 +0800

    little polish

--- .local/share/dwm/autostart.sh
+++ .local/share/dwm/autostart.sh
@@ -2,5 +2,6 @@
 sxhkd -c ~/.config/sxhkd/sxhkdrc&
 picom --experimental-backends -b
 fcitx5 -d --disable wayland,waylandim --enable keyboard,clipboard,unicode,xim,xcb,classui,notificationitem,notificatoins,rime&
+date '+%d' > $HOME/.cache/dwm-date
 dwmblocks&
 # conky&
