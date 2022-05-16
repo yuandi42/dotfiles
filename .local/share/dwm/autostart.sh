6c7729de22d2c1c9aa047bb486504601903eee41
yuandi42 <realyuandi42@gmail.com>
2022-05-16 23:40:06 +0800

    clean up

--- .local/share/dwm/autostart.sh
+++ .local/share/dwm/autostart.sh
@@ -2,6 +2,7 @@
 sxhkd -c ~/.config/sxhkd/sxhkdrc&
 picom --experimental-backends -b
 fcitx5 -d --disable wayland,waylandim --enable keyboard,clipboard,unicode,xim,xcb,classui,notificationitem,notificatoins,rime&
-date '+%d' > $HOME/.cache/dwm-date
 dwmblocks&
+transmission-daemon
+emacs --daemon >/dev/null
 # conky&
