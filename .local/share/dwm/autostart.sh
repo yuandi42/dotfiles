606ecaecbe5252eb528af253583835ea6e678c7e
yuandi42 <realyuandi42@gmail.com>
2022-05-19 16:01:30 +0800

    deploy dotfiles on my surface

--- .local/share/dwm/autostart.sh
+++ .local/share/dwm/autostart.sh
@@ -2,6 +2,9 @@
 sxhkd -c ~/.config/sxhkd/sxhkdrc&
 picom --experimental-backends -b
 fcitx5 -d --disable wayland,waylandim --enable keyboard,clipboard,unicode,xim,xcb,classui,notificationitem,notificatoins,rime&
+# smart capslock
+setxkbmap -option ctrl:nocaps
+xcape -e 'Control_L=Escape'
 dwmblocks&
 transmission-daemon
 emacs --daemon >/dev/null
