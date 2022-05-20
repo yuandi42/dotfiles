991523f090ed93adfd8853bf45e051b884678496
yuandi42 <realyuandi42@gmail.com>
2022-05-20 16:39:06 +0800

    change autostart order

--- .local/share/dwm/autostart.sh
+++ .local/share/dwm/autostart.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 sxhkd -c ~/.config/sxhkd/sxhkdrc&
 picom --experimental-backends -b
-# fcitx5 -d --disable wayland,waylandim --enable keyboard,clipboard,unicode,xim,xcb,classui,notificationitem,notificatoins,rime&
-fcitx5-remote&
 # smart capslock
 setxkbmap -option ctrl:nocaps
 xcape -e 'Control_L=Escape'
+fcitx5-remote&
 dwmblocks&
 transmission-daemon
 emacs --daemon >/dev/null
