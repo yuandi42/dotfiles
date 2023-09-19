1ffaa121883656bcae8aedc4614d181c31de0623
yuandi42 <realyuandi42@gmail.com>
2023-09-19 14:52:17 +0800

    install script reconstruction

--- setup.sh
+++ setup.sh
@@ -2,43 +2,31 @@
 
 set -eu
 ALL="X11 shell misc"
-CONFIGDIR="${HOME}/.config"
-DATADIR="${HOME}/.local/share"
-BINDIR="${HOME}/.local/bin"
-STOWFLAGS="--verbose --target=${HOME} --dir=$(pwd) --dotfiles"
-
-install(){
-	[ -z "$*" ] && echo "Error: No args" && exit 1
-	mkdir -pv "$CONFIGDIR" "$BINDIR" "$DATADIR"
-	for dir in "$@"; do
-		case "$dir" in
-			all) echo "$STOWFLAGS" "$ALL" | xargs stow && exit 0;;
-			X11|shell|misc) echo "$STOWFLAGS" "$dir" | xargs stow;;
-			*) echo "$dir is illegal.";;
-		esac
-	done
-}
-
-uninstall(){
-	[ -z "$*" ] && echo "Error: No args" && exit 1
-	for dir in "$@"; do
-		case "$dir" in
-			all) echo "$STOWFLAGS -D" "$ALL" | xargs stow && exit 0;;
-			X11|shell|misc) echo "$STOWFLAGS -D" "$dir" | xargs stow;;
-			*) echo "$dir is illegal.";;
-		esac
-	done
-}
+STOWFLAGS="--verbose --target=${HOME} --dir=$(dirname "${0}") --dotfiles"
 
 print_help(){
-	echo "Usage: $0 [install|uninstall|help] dirname" >&2
+	echo "Usage: $(basename "${0}") [install|uninstall|help] [$(echo "$ALL"|tr ' ' '|')|all]" >&2
 }
 
-[ -z "$*" ] && echo "Error: No args. Type -h for help message." && exit 1
+install(){
+	[ -z "$*" ] && print_help && exit 1
+	for dir in "$@"; do
+		if echo "all All ALL"|grep -Fq "$dir"; then
+			echo "$STOWFLAGS" "$ALL" | xargs stow && exit 0
+		elif echo "$ALL"|grep -Fq "$dir"; then
+			echo "$STOWFLAGS" "$dir" | xargs stow
+		else
+			echo "$dir is illegal."
+		fi
+	done
+}
+
+[ $# -eq 0 ] && print_help && exit 1
+
 case "$1" in
-	install) shift; install "$*" ;;
-	uninstall) shift; uninstall "$*" ;;
-	help|--help|-h) print_help ;;
-	*) echo "Invaild arg. Type -h for help message." >&2 ;;
+	install|i) shift; install "$@" ;;
+	uninstall|u) shift;  STOWFLAGS="$STOWFLAGS -D"; install "$@" ;;
+	help|h|--help|-h) print_help ;;
+	*) print_help ;;
 esac
 exit 0
